fb491cfad59fc97b28f473570a4085f3ac18037e
GUO,YUE (K-China,ex1) <yue_guo@keysight.com>
2017-11-24 14:04:58 +0800

    initial version

---
+++ helloci/Program.cs
@@ -0,0 +1,19 @@
+﻿using hellocilib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace helloci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HelloWorld hw = new HelloWorld();
+            Console.WriteLine( hw.hello("Isaac"));
+
+        }
+    }
+}
---
+++ hellocilib/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hellocilib
+{
+    public class HelloWorld
+    {
+        public string hello(String name)
+        {
+            return "Hello " + name;
+        }
+    }
+}
